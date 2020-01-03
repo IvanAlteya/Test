1f2456d6bd667af25cc6c2e112441e31bbd4eb51
IvanAlteya <58694345+IvanAlteya@users.noreply.github.com>
2020-01-03 07:12:20 +0200

    Update show.php

--- show.php
+++ show.php
@@ -1,11 +1,28 @@
-<?php get_header(); ?>
-<div class="master">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <h1><?php the_title(); ?></h1>
-            <div class="content"><?php the_content(); ?></div>
-        <?php endwhile;
-     endif; ?>
-</div> 
-<?php get_sidebar(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<?php get_footer(); ?>
+  <div class="conatiner">
+  <div class="container-fluid p-0">
+  <div class="row no-gutters">
+    <div class="col-md-6 post-wrapper">
+      <div class="post featured w-100">
+          <div class="content">
+            <div class="title">
+              <h3> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a></h3>
+            </div>
+            <div class="meta">
+              <span class="category">
+                <i class="fa fa-clock"></i>
+               <?php the_date();?>
+              </span>
+              <span class="comments">
+                <i class="fa fa-comment"></i>
+                0
+              </span>
+            </div>
+          </div>
+        </div>
+    </div>   
+  </div>
+</div>
+  </div>
+  <?php endwhile; endif; ?>
